361a6397d6bdd593176266d25e756dc2f3acdf4b
seyedbol <48900365+seyedbol@users.noreply.github.com>
2019-03-26 10:32:28 -0600

    ICH volume added as a feature,ICH difference is modified

--- linear_regression.py
+++ linear_regression.py
@@ -29,7 +29,7 @@ def gradient_descent(X,y,theta,alpha,num_iters):
        
 np.set_printoptions(threshold=sys.maxsize,precision = 2,linewidth=200)
 MAXROW = 76
-SELECTED = ['E','F','G','H','K','L','N','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AZ']
+SELECTED = ['E','D','F','G','H','K','L','N','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AZ']
 std_col = ['G','J','M','AI','AL','AO']
 wb = opxl.load_workbook("linear_table_edited.xlsx")
 data_set = wb['Sheet3']
@@ -50,21 +50,24 @@ X_matrix = np.matrix(X_matrix)
 y_zero = []
 for y in range(2,MAXROW+1):
     if y!=8 and y!=13 and y!=23 and y!=32 and y!=44 and y!=50 and y!=53 and y!=55 and y!= 58 and y!= 67:
+        """
         cell_idx = 'G'+str(y)
         IPH_0hr = float(std_sheet[cell_idx].value)
         cell_idx = 'J'+str(y)
         IVH_0hr = float(std_sheet[cell_idx].value)
+        """
         cell_idx = 'M'+str(y)
         ICH_0hr = float(std_sheet[cell_idx].value)
-        
+        """
         cell_idx = 'AI'+str(y)
         IPH_24hr = float(std_sheet[cell_idx].value)
         cell_idx = 'AL'+str(y)
         IVH_24hr = float(std_sheet[cell_idx].value)
+        """
         cell_idx = 'AO'+str(y)
         ICH_24hr = float(std_sheet[cell_idx].value)
 
-        diff = IPH_24hr+IVH_24hr+ICH_24hr-(IPH_0hr+IVH_0hr+ICH_0hr)
+        diff = ICH_24hr-(ICH_0hr)
         y_zero.append(diff)
         # print("row: ",y)
         # print(diff)
@@ -77,13 +80,13 @@ y_zero = np.transpose(y_zero)
 print("Y dimension:",y_zero.shape)
 # print(y_zero.shape)
 
-theta = np.full((24,1),1)
+theta = np.full((25,1),1)
 print(theta.shape)
 
 ones = np.full((65,1),1)
 X_matrix = np.hstack((ones,X_matrix))
 #print(X_matrix)
-alpha=0.00001
+alpha=0.0000001
 num_iters=10000
 
 (J_history,theta)= gradient_descent(X_matrix,y_zero,theta,alpha,num_iters)
