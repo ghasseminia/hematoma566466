58e540871dc700d1c1db1aa9b6b76b97f6029430
Jordan McCarthy <jmmccart@ualberta.ca>
2019-04-09 15:40:01 -0600

    finished SVM

--- SVM.py
+++ SVM.py
@@ -23,11 +23,30 @@ def SimilarityMatrix(prediction,truth):
 		# false negative (predicted negative but is positive)
 		elif prediction[i] == 0 and truth[i] == 1:
 			matrix[1][0] += 1
-	print("true negatives: ", matrix[1][1])
-	print("false negatives: ", matrix[1][0])
-	print("true positive: ", matrix[0][0])
-	print("false positives: ", matrix[0][1])
+	print("         -----------------------")
+	print("         --------True values----")
+	print("------------Class 1----Class 0--")
+	print("-P|-----------------------------")
+	print("-r|---------   TP  ----   FP  --")
+	print("-e|Class 1--   " + str(matrix[0][0]) + "   ----   "+ str(matrix[0][1])+ "   --")
+	print("-d|-----------------------------")   
+	print("-i|-----------------------------")
+	print("-c|---------   FN  ----   TN  --")
+	print("-t|Class 0--   " + str(matrix[1][0]) + "   ----   "+ str(matrix[1][1])+ "   --")
+	print("------------------------------")
+	#print("true negatives: ", matrix[1][1])
+	#print("false negatives: ", matrix[1][0])
+	#print("true positive: ", matrix[0][0])
+	#print("false positives: ", matrix[0][1])
+	print("\n")
 	return matrix
+	
+def GetSplitData(size, datax,datay):
+	trainx = np.matrix(datax[:size,:])
+	testx = np.matrix(datax[size:,:])
+	trainy = np.matrix(datay[:size])
+	testy = np.matrix(datay[size:])
+	return trainx, trainy, testx, testy 
 	
 	
 	
@@ -110,7 +129,7 @@ for y in range(2,MAXROW+1):
             #print(x+str(y))
             cell_idx = x+str(y)
             # print(cell_idx)
-            print(data_set[cell_idx].value)
+            #print(data_set[cell_idx].value)
             row.append(float(data_set[cell_idx].value))
         # print(row)
         X_matrix.append(row)
@@ -132,7 +151,7 @@ for y in range(2,MAXROW+1):
         y_zero.append(diff)
     
     
-print(X_matrix)
+#print(X_matrix)
 initial_volumes = np.matrix(initial_volumes)
 initial_volumes = np.transpose(initial_volumes)
 end_volumes = np.matrix(end_volumes)
@@ -154,44 +173,49 @@ min = 100
 minC = 0
 dice = 0
 percent = 0
+# split data, testSize and totalSize - testSize 
+trainx, trainy, testx, testy = GetSplitData(50, X_matrix,np.array(y_binary))
+trainy = np.array(trainy.tolist()[0])
+testy = np.array(testy.tolist()[0])
 
 
-for i in range(15):
-	# score function: twice iterated 5-fold cross-validated accuracy
-	@optunity.cross_validated(x=X_matrix, y=y_binary, num_folds=5, num_iter=2)
-	def svm_auc(x_train, y_train, x_test, y_test, logC, logGamma):
-		model = sklearn.svm.SVC(C=10 ** logC, gamma=10 ** logGamma,kernel='rbf').fit(x_train, y_train)
-		decision_values = model.decision_function(x_test)
-		return optunity.metrics.roc_auc(y_test, decision_values)
-
-	# perform tuning
-	hps, _, _ = optunity.maximize(svm_auc, num_evals=200, logC=[-4, 10], logGamma=[-100, 100])
 
 
-	# train model on the full training set with tuned hyperparameters
-	optimal_model = sklearn.svm.SVC(C=10 ** hps['logC'], gamma=10 ** hps['logGamma']).fit(X_matrix, y_binary)
+# score function: twice iterated 5-fold cross-validated accuracy
+@optunity.cross_validated(x=trainx, y=trainy, num_folds=4, num_iter=2)
+def svm_auc(x_train, y_train, x_test, y_test, logC, logGamma):
+	model = sklearn.svm.SVC(C=10 ** logC, gamma=10 ** logGamma,kernel='rbf').fit(x_train, y_train)
+	decision_values = model.decision_function(x_test)
+	return optunity.metrics.roc_auc(y_test, decision_values)
 
-	output = []
-	ourguess = []
-	for j in range(65):
-		test = optimal_model.predict(X_matrix[j,:])
-		ourguess.append(test[0])
+# perform tuning
+hps, _, _ = optunity.maximize(svm_auc, num_evals=50, logC=[-10, 10], logGamma=[-100, 100])
+print(hps)
+
+# train model on the full training set with tuned hyperparameters
+optimal_model = sklearn.svm.SVC(C=3.1, gamma=0.00025).fit(X_matrix, y_binary)
+
+output = []
+ourguess = []
+for j in range(15):
+	test = optimal_model.predict(testx[j,:])
+	ourguess.append(test[0])
 			
-	ourguess = np.array(ourguess)
-	binary = []
+ourguess = np.array(ourguess)
+binary = []
 
 
-	for i in range(len(ourguess)):
-		binary.append(ourguess[i]-y_binary[i])
-	percent += accuracy(binary)
+for i in range(len(ourguess)):
+	binary.append(ourguess[i]-testy[i])
+percent = accuracy(binary)
 
-	simMatrix = SimilarityMatrix(ourguess,y_binary)
-	#print("percent: " + str(percent))
-	dice += dice_coefficient(simMatrix)
-	#jaccardCoefficient(simMatrix)
+simMatrix = SimilarityMatrix(ourguess,testy)
+#print(simMatrix)
+dice = dice_coefficient(simMatrix)
 
 
-print("percent: " + str(percent/15))
-print("Dice: " + str(dice/15))
+
+print("percent: " + str(percent))
+print("Dice: " + str(dice))
 
 
