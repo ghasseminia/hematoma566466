af00e0079c7249024e5d74d8cf3119980e2c8be9
DFangsaur <daniel.fangtz@gmail.com>
2019-04-14 23:06:19 -0600

    added parameter calculation and readme

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# hematoma566466
-#test
+After testing, the best parameter used for mortality and expansion modeling are: 
+mortality: max_depth = 10, min_samples_leaf = 15, max_leaf_nodes = 4 (gini)
+           max_depth = 3, min_samples_leaf = 6, max_leaf_nodes = 4 (entropy)
+expansion: max_depth = 10, min_samples_leaf = 6, max_leaf_nodes = 8 (gini)
+           max_depth = 3, min_samples_leaf = 8, max_leaf_nodes = 8 (entropy)
+
+
+----mortality result after t=10000:----
+Gini:
+average accuracy:  84.93000000000059
+average accuracy if choosing the most common:  85.29533333333326
+for  15  samples in test, there are on average  0.9915 false_positives
+for  15  samples in test, there are on average  1.269 false_negatives
+for  15  samples in test, there are on average  0.9367 correct_positives
+for  15  samples in test, there are on average  11.8028 correct_negatives
+the top ten most important features are:
+- Heart Rate  0.5745296002741171
+- Acute_IVH_MEAN 0.13112233528851205
+- Mortality 0.09767724050751962
+- Time Sx onset - CTP (hours) 0.0581779593389712
+- Baseline Systolic BP 0.046882902049974993
+- Gender 0.0251223305781502
+- 2 hour GCS 0.023741175778570944
+- Enalapril 0.016086849389144897
+-----------------------------------------
+Entropy:
+average accuracy:  82.05000000000014
+average accuracy if choosing the most common:  85.43066666666638
+for  15  samples in test, there are on average  1.1477 false_positives
+for  15  samples in test, there are on average  1.5448 false_negatives
+for  15  samples in test, there are on average  0.6406 correct_positives
+for  15  samples in test, there are on average  11.6669 correct_negatives
+the top ten most important features are:
+- Acute_IVH_MEAN 0.46109005891617855
+- Heart Rate  0.21281916186739264
+- Baseline Systolic BP 0.08469684132373309
+- Anticoag (Y/N) 0.05201698622378152
+- Mortality 0.047301242470451164
+- Time Sx onset - CTP (hours) 0.03598483307775689
+- Baseline GCS 0.021356301249414143
+- Gender 0.018241271193128968
+
+
+----expansion result after t=10000:----
+Gini:
+average accuracy:  77.58785714285966
+average accuracy if choosing the most common:  74.38857142857154
+for  14  samples in test, there are on average  1.0885 false_positives
+for  14  samples in test, there are on average  2.0492 false_negatives
+for  14  samples in test, there are on average  1.5364 correct_positives
+for  14  samples in test, there are on average  9.3259 correct_negatives
+the top ten most important features are:
+- 2 hour NIHSS 0.5936993502180039
+- Time Sx Onset - Randomization (hours) 0.10578946179360084
+- Baseline NIHSS 0.0734228298476119
+- Acute_IPH_vol_MEAN 0.06354800852081374
+- Baseline Systolic BP 0.03315251342357231
+- Baseline Diastolic BP 0.029959007519679358
+- Baseline MAP 0.026626387555978546
+- Age 0.020634289056221552
+-----------------------------------------
+Entropy:
+average accuracy:  75.51785714285874
+average accuracy if choosing the most common:  74.39214285714337
+for  14  samples in test, there are on average  1.22 false_positives
+for  14  samples in test, there are on average  2.2075 false_negatives
+for  14  samples in test, there are on average  1.3776 correct_positives
+for  14  samples in test, there are on average  9.1949 correct_negatives
+the top ten most important features are:
+- 2 hour NIHSS 0.4328229801118633
+- Baseline NIHSS 0.15862976099271694
+- Time Sx Onset - Randomization (hours) 0.11207640722485752
+- Acute_IPH_vol_MEAN 0.04354677988791732
+- Baseline Systolic BP 0.03804463913563131
+- Baseline Diastolic BP 0.0356249854650101
+- Baseline MAP 0.03205929840530993
+- Age 0.0313294846726127
--- decision_tree.py
+++ decision_tree.py
@@ -11,14 +11,18 @@ np.set_printoptions(threshold=sys.maxsize)
 
 #--import the data--#########################
 def import_data(filename):
-    data = pd.read_excel(filename)
+    data = pd.read_excel(filename,sheet_name = 1)
     # print("data length:", len(data))
     # print("data shape:",data.shape)
 
     # print("dataset:", data.head())
     return data
 
-
+def impute_data(X):
+    imp = SimpleImputer(missing_values=np.nan, strategy='median')
+    X = imp.fit_transform(X)
+    # print(X)
+    return X
 
 #--Function used to determine X and Y depending on what purpose of result_type-- ########################
 def set_parameter(data,result_type):
@@ -26,6 +30,7 @@ def set_parameter(data,result_type):
     if result_type == "mortality":
         X = data_np[:,3:]
         X = X.astype(float)
+        X = impute_data(X)
         Y = data_np[:,2]
         Y = Y.astype(int)
         # print("Y, ",Y)
@@ -33,10 +38,14 @@ def set_parameter(data,result_type):
     elif result_type == "expansion":
         X = data_np[:,3:]
         X = X.astype(float)
+        X = impute_data(X)
         final_ICH = data_np[:,2]
-        acute_total = data_np[:,3:4]
+        acute_total = data_np[:,3:5]
+        # print(acute_total)
         acute_total = np.sum(acute_total,1)
+        # print(acute_total)
         acute_total = acute_total*1.3
+        # print(acute_total)
         Y = np.zeros(np.shape(final_ICH))
         for i in range(np.size(acute_total)):
             if final_ICH[i]>=acute_total[i]:
@@ -54,34 +63,40 @@ def cross_validation_split(X,Y):
 
 
 #--Function to perform training with giniIndex-- #################################
-def train_gini(X_train, X_test, Y_train): 
+def train_gini(X_train, Y_train,md,msp,msl,mlf): 
   
     # Creating the classifier object 
-    clf_gini = DecisionTreeClassifier(criterion = "gini",max_depth = 4, min_samples_split = 17, min_samples_leaf = 17) 
+    clf_gini = DecisionTreeClassifier(criterion = "gini",max_depth = md, min_samples_split = msp, min_samples_leaf = msl,max_leaf_nodes=mlf) 
     # print(clf_gini)
     # Performing training 
     clf_gini.fit(X_train,Y_train) 
     return clf_gini 
 
 #--Function to perform training with entropy-- #################################
-def train_entropy(X_train, X_test, y_train): 
+def train_entropy(X_train, y_train,md,msp,msl,mlf): 
   
     # Decision tree with entropy 
-    clf_entropy = DecisionTreeClassifier(criterion = "entropy", max_depth = 4, min_samples_split =17, min_samples_leaf = 17) 
+    clf_entropy = DecisionTreeClassifier(criterion = "entropy", max_depth = md, min_samples_split =msp, min_samples_leaf = msl,max_leaf_nodes=mlf) 
   
     # Performing training 
     clf_entropy.fit(X_train, y_train) 
     return clf_entropy 
 
 #--function to produce all the outcome that i need==#####################
-def produce_outcome(X,Y,data,test_type,t,result_type):
+def produce_outcome(X,Y,data,test_type,t,result_type,md,msp,msl,mlf):
     
   
+    # comparison = []
+    # for md in range(3,12):
+    #     for msp in range(3,16):
+    #         for msl in range(3,16):
+    #             print (md,msp,msl)
     overall_acc = 0
-    overall_defauly_acc = 0
+    overall_default_acc = 0
     overall_false_positive = 0
     overall_false_negative = 0
-
+    overall_correct_positive = 0
+    overall_correct_negative = 0
     #the overall most impactful features
     feature_weight = np.zeros(np.shape(X)[1])
 
@@ -89,9 +104,9 @@ def produce_outcome(X,Y,data,test_type,t,result_type):
         X_train, X_test, Y_train, Y_test = cross_validation_split(X,Y)
         
         if test_type == "gini":
-            clf_obj = train_gini(X_train,X_test,Y_train)
+            clf_obj = train_gini(X_train,Y_train,md,msp,msl,mlf)
         else:
-            clf_obj = train_entropy(X_train,X_test,Y_train)
+            clf_obj = train_entropy(X_train,Y_train,md,msp,msl,mlf)
         # print(clf_obj.tree_)
         Y_pred = clf_obj.predict(X_test) 
         # print(Y_pred)
@@ -104,31 +119,40 @@ def produce_outcome(X,Y,data,test_type,t,result_type):
         #default accuracy when guessing the most common
         if result_type == "mortality":
             default_acc = 100-np.sum(Y_test)/np.size(Y_test)*100
-            overall_defauly_acc+=default_acc
+            overall_default_acc+=default_acc
         elif result_type == "expansion":
             default_acc = 100-np.sum(Y_test)/np.size(Y_test)*100
-            overall_defauly_acc+=default_acc
+            overall_default_acc+=default_acc
         
         #false positive and negative count
         false_positive = 0
         false_negative = 0
+        correct_positive = 0
+        correct_negative = 0
         for j in range(np.size(Y_pred)):
             if Y_pred[j] == 1 and Y_test[j] == 0:
                 false_positive +=1
             elif Y_pred[j] == 0 and Y_test[j] == 1:
                 false_negative +=1
+            elif Y_pred[j] == 1 and Y_test[j] == 1:
+                correct_positive +=1
+            elif Y_pred[j] == 0 and Y_test[j] == 0:
+                correct_negative += 1
         overall_false_positive+=false_positive
         overall_false_negative+=false_negative
+        overall_correct_positive += correct_positive
+        overall_correct_negative += correct_negative
 
         #the weight of each feature
         feature_weight += clf_obj.feature_importances_
 
-        
 
     print("average accuracy: ",overall_acc/t)
-    print("average accuracy if choosing the most common: ",overall_defauly_acc/t)
+    print("average accuracy if choosing the most common: ",overall_default_acc/t)
     print("for ",np.size(Y_test)," samples in test, there are on average ",overall_false_positive/t, "false_positives")
     print("for ",np.size(Y_test)," samples in test, there are on average ",overall_false_negative/t, "false_negatives")
+    print("for ",np.size(Y_test)," samples in test, there are on average ",overall_correct_positive/t, "correct_positives")
+    print("for ",np.size(Y_test)," samples in test, there are on average ",overall_correct_negative/t, "correct_negatives")
 
     #print the most prominent features
     print("the top ten most important features are: ")
@@ -136,10 +160,34 @@ def produce_outcome(X,Y,data,test_type,t,result_type):
     top =  np.flip(top)
     if result_type == "mortality":
         for idx in top:
-            print(data.columns[idx+2],feature_weight[idx]/t)
+            print("-",data.columns[idx+2],feature_weight[idx]/t)
     elif result_type == "expansion":
         for idx in top:
-            print(data.columns[idx+3],feature_weight[idx]/t)
+            print("-",data.columns[idx+3],feature_weight[idx]/t)
+    
+    return overall_acc/t,overall_default_acc/t
+    
+
+#--function to search for the best parameter ==#####################
+def find_parameters(X,Y,data,test_type,t,result_type):
+    
+    #calculate parameters for gini
+    comparison = []
+    for md in range(3,12):
+        for msp in range(3,16):
+            for msl in range(3,16):
+                mlf = None
+                # print (md,msp,msl)
+                calculated_acc,default_acc = produce_outcome(X,Y,data,test_type,t,result_type,md,msp,msl,mlf)
+                comparison.append([md,msp,msl,mlf,calculated_acc,default_acc])
+        
+    only_acc = np.array(comparison)[:,4]
+    # print(only_acc)
+    only_acc_idx = np.argsort(only_acc)
+    only_acc_idx = np.flip(only_acc_idx)
+    for idx in only_acc_idx:
+        print(comparison[idx])    
+
 #--main--###########################################
 def main():
     # input data format requirement:
@@ -155,11 +203,17 @@ def main():
     except:
         print("wrong input type")
     
+    
+    
+
     print("Gini: ")
-    produce_outcome(X, Y,data,"gini",t,sys.argv[3])
+    # find_parameters(X, Y,data,"gini",t,sys.argv[3])
+    produce_outcome(X, Y,data,"gini",t,sys.argv[3],10,6,8,None)
     print("-----------------------------------------")
     print("Entropy: ")
-    produce_outcome(X, Y,data,"entropy",t,sys.argv[3])
+    # find_parameters(X, Y,data,"entropy",t,sys.argv[3])
+    produce_outcome(X, Y,data,"entropy",t,sys.argv[3],3,8,8,None)
+
     
 if __name__=="__main__": 
     main() 
